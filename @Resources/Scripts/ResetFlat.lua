a621240d0542468638af4cf6f5d1e9f8b50dcc14
Evans Stenakis <ervans589@gmail.com>
2021-05-31 14:57:25 +0300

    Updated to 21.06.01

--- @Resources/Scripts/ResetFlat.lua
+++ @Resources/Scripts/ResetFlat.lua
@@ -1,7 +1,7 @@
 function ResetFlat()
-	SKIN:Bang('!WriteKeyValue Variables MColor "114,86,115" "#@#Systemvariables.inc"')
-	SKIN:Bang('!WriteKeyValue Variables NColor "72,119,135" "#@#Systemvariables.inc"')
-	SKIN:Bang('!WriteKeyValue Variables AColor "85,145,107" "#@#Systemvariables.inc"')
-	SKIN:Bang('!WriteKeyValue Variables EColor "194,86,48" "#@#Systemvariables.inc"')
-	SKIN:Bang('!WriteKeyValue Variables NTColor "136,51,110" "#@#Systemvariables.inc"')
+	SKIN:Bang('!WriteKeyValue Variables MColor "105,166,98" "#@#Systemvariables.inc"')
+	SKIN:Bang('!WriteKeyValue Variables NColor "82,135,153" "#@#Systemvariables.inc"')
+	SKIN:Bang('!WriteKeyValue Variables AColor "51,142,133" "#@#Systemvariables.inc"')
+	SKIN:Bang('!WriteKeyValue Variables EColor "143,52,59" "#@#Systemvariables.inc"')
+	SKIN:Bang('!WriteKeyValue Variables NTColor "150,50,78" "#@#Systemvariables.inc"')
 end
